4a899e0a26bf43a13d22dc31df6304a29e74806a
Michael Hatch <michaelhatch@me.com>
2025-07-31 13:25:04 -0700

    add maxMoves config option

--- js/ui.js
+++ js/ui.js
@@ -39,6 +39,7 @@ function init() {
     generateMoves: game.generateMoves,
     getPlayerFromState: true,
     isGameOver: game.isGameOver,
+    maxMoves: 6,
   });
   document.querySelectorAll(".row").forEach((row, ri) => {
     Array.from(row.children).forEach((cell, ci) => {
